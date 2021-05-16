f7987dc253b7d34ccf8ce9a1b4606b77cfb7fe18
Raghav Thakar <raghavthakar12@gmail.com>
2021-05-16 19:36:50 +0530

    Make graph of map, visualise in python

--- scripts/visualiser.py
+++ scripts/visualiser.py
@@ -9,9 +9,9 @@ pygame.init()
 # Creating the screen.
 screen=pygame.display.set_mode((1000, 1000))
 #Setting the background colour
-screen.fill((200, 40, 40))
+screen.fill((50, 0, 50))
 # Title and icon
-pygame.display.set_caption("RRT")
+pygame.display.set_caption("PRM")
 
 # Function to show obstacle on Screen
 # -----------------------SHOW OBSTACLE------------------------------------------
@@ -58,40 +58,48 @@ def showTargetBox(col, row, radius):
 def showEdge(node_info, colour, width):
     col=int(node_info[0])
     row=int(node_info[1])
-    parent_col=int(node_info[2])
-    parent_row=int(node_info[3])
-
-    pygame.draw.line(screen, colour, (col, row), (parent_col, parent_row), width)
+    # parent_col=int(node_info[2])
+    # parent_row=int(node_info[3])
+    i=2
+    while True:
+        try:
+            # Every alternate node_info entry is the row/col
+            neighbour_col=int(node_info[i])
+            neighbour_row=int(node_info[i+1])
+            pygame.draw.line(screen, colour, (col, row), (neighbour_col, neighbour_row), width)
+            i+=2;
+        except:
+            break
 
 # ----------------------MAIN SEQUENCE-------------------------------------------
 print("Hello World!")
 running = True
 
 #-----------------------get obstacle from csv-----------------------------------
-with open('../src/map.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter = ',')
-
-    starting_point_info=next(reader)
-    showSpecialDot(starting_point_info[0], starting_point_info[1]);
-
-    target_point_info=next(reader)
-
-    target_radius_info=next(reader)
-    showTargetBox(target_point_info[0], target_point_info[1], target_radius_info[0])
-
-    # Target dot shown after radius so that it's over it
-    showSpecialDot(target_point_info[0], target_point_info[1]);
-
-    while(True):
-        try:
-            obstacle_info=next(reader)
-            showObstacle(obstacle_info)
-        except:
-            break
+# with open('../src/map.csv', 'r') as csv_file:
+#     reader = csv.reader(csv_file, delimiter = ',')
+#
+#     starting_point_info=next(reader)
+#     showSpecialDot(starting_point_info[0], starting_point_info[1]);
+#
+#     target_point_info=next(reader)
+#
+#     target_radius_info=next(reader)
+#     showTargetBox(target_point_info[0], target_point_info[1], target_radius_info[0])
+#
+#     # Target dot shown after radius so that it's over it
+#     showSpecialDot(target_point_info[0], target_point_info[1]);
+#
+#     while(True):
+#         try:
+#             obstacle_info=next(reader)
+#             showObstacle(obstacle_info)
+#         except:
+#             break
 
 #-----------------------csv file reading-------------------
 #Execute the main loop, reading one row of csv every iteration
-with open('../src/tree.csv', 'r') as csv_file:
+with open('../src/graph.csv', 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter = ',')
 
     while running:
@@ -101,33 +109,33 @@ with open('../src/tree.csv', 'r') as csv_file:
 
         try:
             node_info = next(reader)
-            # Show a dot at node location
-            showDot(node_info)
             #show an edge from node to parent node
             showEdge(node_info, (100, 200, 150), 2)
+            # Show a dot at node location
+            showDot(node_info)
 
         except:
-            break;
-
-        pygame.display.flip()
-        # time.sleep(0.1)
-
-#--------------------path display--------------------------------
-with open('../src/path.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter = ',')
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running=False
-
-        try:
-            node_info = next(reader)
-            #show an edge from node to parent node
-            showEdge(node_info, (0, 50, 0), 5)
-
-        except:
-            pass
+            pass;
 
         pygame.display.flip()
         time.sleep(0.1)
+
+#--------------------path display--------------------------------
+# with open('../src/path.csv', 'r') as csv_file:
+#     reader = csv.reader(csv_file, delimiter = ',')
+#
+#     while running:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running=False
+#
+#         try:
+#             node_info = next(reader)
+#             #show an edge from node to parent node
+#             showEdge(node_info, (0, 50, 0), 5)
+#
+#         except:
+#             pass
+#
+#         pygame.display.flip()
+#         time.sleep(0.1)
--- src/prm.cpp
+++ src/prm.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
-#include<list>
-#include<random>
-#include<chrono>
-#include<cmath>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <random>
+#include <chrono>
+#include <cmath>
+#include <unistd.h>
 
 #define MAP_LENGTH 1000
-#define CLUSTER_RADIUS 2000
+#define CLUSTER_RADIUS 100
 #define CLUSTER_NUMBER 5
 
 using namespace std;
@@ -19,7 +21,7 @@ class Node
   //This list stores pointer to all connected nodes
   list<Node*> connections;
 
-public:
+  public:
   //Constructor
   Node(int col, int row)
   {
@@ -39,31 +41,75 @@ public:
     return row;
   }
 
+  void writeToCSV()
+  {
+    ofstream csv_file;
+    csv_file.open("graph.csv", ios::app);
+
+    csv_file<<getCol()<<","<<getRow()<<",";
+
+    for(list<Node*>::iterator it=connections.begin();
+        it!=connections.end(); it++)
+    {
+      csv_file<<(*it)->getCol()<<","<<(*it)->getRow()<<",";
+    }
+    csv_file<<endl;
+
+    csv_file.close();
+  }
+
   //Add connection
   void addConnection(Node* connection)
   {
     connections.push_back(connection);
   }
 
+  //Returns an iterator pointing to start of list
+  list<Node*>::iterator getConnectionsBegin()
+  {
+    return connections.begin();
+  }
+
+  //Returns an iterator pointing to end of list
+  list<Node*>::iterator getConnectionsEnd()
+  {
+    return connections.end();
+  }
+
+  //Get number of connections
+  int getNumberOfConnections()
+  {
+    return connections.size();
+  }
+
   //Display info of the node
   void display()
   {
-    cout<<"Column is: "<<col<<endl;
-    cout<<"Row is: "<<row<<endl;
+    cout<<"Column is: "<<getCol()<<endl;
+    cout<<"Row is: "<<getRow()<<endl;
   }
 
+  //Displays info in comapct manner
   void displayMinimal()
   {
     cout<<col<<","<<row;
   }
 
+  //Displays all of the connection of a node
   void displayConnections()
   {
-    for(list<Node*>::iterator it=connections.begin();
-        it!=connections.end(); it++)
+    if(getNumberOfConnections())
     {
-      (*it)->displayMinimal();
-      cout<<"\t";
+      for(list<Node*>::iterator it=connections.begin();
+          it!=connections.end(); it++)
+      {
+        (*it)->displayMinimal();
+        cout<<"\t";
+      }
+    }
+    else
+    {
+      cout<<"Bruh";
     }
   }
 };
@@ -79,6 +125,14 @@ class Graph
   {
     return sqrt(pow(n1.getCol()-n2.getCol(), 2)
             +pow(n1.getRow()-n2.getRow(), 2));
+  }
+
+  // Clears the CSV files
+  void clearCSV()
+  {
+    ofstream csv_file;
+    csv_file.open("graph.csv");
+    csv_file.close();
   }
 
   //Generates a random configuration in the workspace
@@ -100,6 +154,7 @@ class Graph
   //make edges in cluster
   void createCluster(Node* x_rand)
   {
+    //make edge bw x_rand and all neighbouring nodes
     for(list<Node>::iterator i=all_nodes.begin();
         i!=all_nodes.end(); i++)
     {
@@ -109,8 +164,25 @@ class Graph
         x_rand->addConnection(&(*i));
       }
     }
+
+    //if there are nodes with no edges then force make on with nearest node
+    list<Node>::iterator nearest=all_nodes.begin();
+    //find nearest node
+    if(!x_rand->getNumberOfConnections())
+    {
+      for(list<Node>::iterator i=all_nodes.begin();
+          i!=all_nodes.end(); i++)
+      {
+        if(distanceBetween(*i, *x_rand)<distanceBetween(*nearest, *x_rand))
+          nearest=i;
+      }
+      //make edge with nearest node
+      nearest->addConnection(x_rand);
+      x_rand->addConnection(&(*nearest));
+    }
   }
 
+  //add the node to the graph
   void addNode(Node* x_rand)
   {
     //Push the random node into list of all nodes
@@ -120,14 +192,18 @@ class Graph
 
 int main()
 {
+  usleep(1000);
   Graph main_graph;
 
   Node* x_root;
   Node* x_rand;
 
+  main_graph.clearCSV();
+
   //main loop
-  for(int i=0; i<7; i++)
+  for(int i=0; i<500; i++)
   {
+    cout<<i;
     //randomly smaple a point on the workspace
     x_rand = main_graph.generateRandomConfig();
     cout<<"The randomly sampled point is: \n";
@@ -142,18 +218,20 @@ int main()
     //if not illegal then simply add to graph and connect to neighbours
     else
     {
+      //add edges with neighbours
       main_graph.createCluster(x_rand);
+      //add to list of all nodes
       main_graph.addNode(x_rand);
     }
   }
 
-  //show connections of all nodes
+  //Loop to write node data into csv file
   for(list<Node>::iterator it=main_graph.all_nodes.begin();
       it!=main_graph.all_nodes.end(); it++)
   {
-    (*it).displayConnections();
-    cout<<endl;
+    (*it).writeToCSV();
   }
 
+  
   return 0;
 }
